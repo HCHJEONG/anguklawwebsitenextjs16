bdafdc6df25e60da3811eb6238863d5b38c4b311
HCHJEONG <hcjeong@kakao.com>
2026-04-23 17:50:06 +0900

    anguk logo tsx

--- app/members/sidenav.tsx
+++ app/members/sidenav.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import NavLinks from './nav-links';
-import AcmeLogo from '@/app/ui/acme-logo';
+import AngukLogo from '@/app/ui/anguk-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
 
 export default function SideNav() {
@@ -11,7 +11,7 @@ export default function SideNav() {
         href="/"
       >
         <div className="w-32 text-white md:w-40">
-          <AcmeLogo />
+          <AngukLogo />
         </div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
--- app/ui/anguk-logo.tsx
+++ app/ui/anguk-logo.tsx
@@ -7,7 +7,7 @@ export default function AcmeLogo() {
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[28px] block md:hidden">Anguk Law Offices</p>
+      <p className="text-[20px] block md:hidden">Anguk Law Offices</p>
       <p className="text-[44px] hidden md:block ml-4">Anguk Law Offices</p>
     </div>
   );
