2b7889036fb67efe9afed2bcd6612f010f66008d
HCHJEONG <hcjeong@kakao.com>
2026-04-24 19:30:20 +0900

    /** @type {import('next').NextConfig} */ const nextConfig = {   output: 'standalone',   transpilePackages: ["three"], };
    export default nextConfig;

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
-import type { NextConfig } from 'next';
+// import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
   
+// };
+
+// export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  transpilePackages: ["three"],
 };
 
 export default nextConfig;
